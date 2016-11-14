277b95fd3c8e0147d3cd3f98f6a9370ec3e13b27
Tongxi Lou <tongxi.lou@tum.de>
2016-11-14 19:41:57 +0100

    parameter for io

--- src/ros_kinfu_solo_node.cpp
+++ src/ros_kinfu_solo_node.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
 
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/fill_image.h>
 
 #include <pcl/gpu/kinfu_large_scale/kinfu.h>
@@ -29,6 +31,7 @@ private:
 
     ros::Subscriber _sub_depth;
     ros::Subscriber _sub_color;
+    std::string _topic_view; // empty if disable
 
     void execute()
     {
@@ -52,17 +55,7 @@ private:
                 has_image = (*_kinfu)(_depth_device);
             }
 
-            // publish image, will be moved to another place
-            ns_kinfuls::KinfuTracker::View view_device;
-            std::vector<ns_kinfuls::PixelRGB> view_host;
-            int cols;
-            static ros::Publisher pub = _nh.advertise<sensor_msgs::Image>("/scene", 1, true);
-            sensor_msgs::ImagePtr msg = sensor_msgs::ImagePtr(new sensor_msgs::Image());
-            _kinfu->getImage(view_device);
-            view_device.download(view_host, cols);
-            sensor_msgs::fillImage(*msg, "rgb8", view_device.rows(), view_device.cols(),
-                                   view_device.cols() * 3, &view_host.front());
-            pub.publish(msg);
+            publishView(_topic_view);
         }
         _depth_ptr.reset();
         _color_ptr.reset();
@@ -88,6 +81,50 @@ private:
         _color_ptr = msg;
     }
 
+    void loadParams()
+    {
+        typedef sensor_msgs::CameraInfo INFO;
+        // directly available parameters
+        _height = (float)_nh.param<double>("height", -1.0);
+        _width = (float)_nh.param<double>("width", -1.0);
+        // topics
+        std::string topic_depth, topic_color, topic_info;
+        topic_depth = _nh.param<std::string>("topic_depth", "");
+        topic_color = _nh.param<std::string>("topic_image", "");
+        topic_info = _nh.param<std::string>("topic_camera_info", "");
+        INFO::ConstPtr info = ros::topic::waitForMessage<INFO>(topic_info, _nh, ros::Duration(2));
+        _topic_view = _nh.param<std::string>("topic_view", "");
+        if(topic_depth.empty() || topic_color.empty() || topic_info.empty()
+                || _height < 0.0f || _width < 0.0f || !info)
+        {
+            ROS_ERROR("parameter error");
+            ros::shutdown();
+        }
+        // should be safe, apply
+        _sub_depth = _nh.subscribe<sensor_msgs::Image>(topic_depth, 1, &KinfuLSApp::cbDepth, this);
+        _sub_color = _nh.subscribe<sensor_msgs::Image>(topic_color, 1, &KinfuLSApp::cbColor, this);
+        _kinfu->setDepthIntrinsics(info->K.at(0), info->K.at(4), info->K.at(2), info->K.at(5));
+    }
+
+    void publishView(const std::string topic)
+    {
+        if(topic.empty())
+        {
+            return;
+        }
+        static ros::Publisher pub = _nh.advertise<sensor_msgs::Image>(topic, 1, true);
+        // publish image, will be moved to another place
+        ns_kinfuls::KinfuTracker::View view_device;
+        std::vector<ns_kinfuls::PixelRGB> view_host;
+        int cols;
+        sensor_msgs::ImagePtr msg = sensor_msgs::ImagePtr(new sensor_msgs::Image());
+        _kinfu->getImage(view_device);
+        view_device.download(view_host, cols);
+        sensor_msgs::fillImage(*msg, "rgb8", view_device.rows(), view_device.cols(),
+                               view_device.cols() * 3, &view_host.front());
+        pub.publish(msg);
+    }
+
 public:
     KinfuLSApp(const float volume_size, const float shift_distance, const int snapshot_rate,
                const float width, const float height):
@@ -95,11 +132,10 @@ public:
         _volume_size(volume_size),
         _shift_distance(shift_distance),
         _snapshot_rate(snapshot_rate),
-        _width(width),
-        _height(height),
         _is_lost(false),
         _kinfu(new ns_kinfuls::KinfuTracker(Eigen::Vector3f::Constant(_volume_size),
-                                            _shift_distance))
+                                            _shift_distance)),
+        _topic_view("")
     {
         // modified from or directly from kinfuLS_app.cpp, pcl 1.8
         Eigen::Matrix3f R = Eigen::Matrix3f::Identity ();   // * AngleAxisf( pcl::deg2rad(-30.f), Vector3f::UnitX());
@@ -118,13 +154,9 @@ public:
         _tsdf_cloud_ptr = pcl::PointCloud<pcl::PointXYZI>::Ptr(new pcl::PointCloud<pcl::PointXYZI>);
         _frame_counter = 0;
 
-        _kinfu->setDepthIntrinsics(574.0527954101562, 574.0527954101562, 314.5, 235.5);
+        loadParams();
 
         boost::thread worker(boost::bind(&KinfuLSApp::loop, this));
-        _sub_depth = _nh.subscribe<sensor_msgs::Image>("/camera/depth/image_raw", 1,
-                                                       &KinfuLSApp::cbDepth, this);
-        _sub_color = _nh.subscribe<sensor_msgs::Image>("/camera/rgb/image_raw", 1,
-                                                       &KinfuLSApp::cbColor, this);
     }
     ~KinfuLSApp()
     {
